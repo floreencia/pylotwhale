7ae455e2612b2566463c3d7431bfe5fc8171ec64
Florencia Noriega <flo@nld.ds.mpg.de>
2016-12-14 17:21:41 +0100

    tools for running call-clf experiments w/ cutted wavs

---
+++ pylotwhale/MLwhales/experiment_callClf.py
@@ -0,0 +1,235 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 18:05:03 2015
+#!/usr/bin/python
+@author: florencia
+
+Runs call classification experiments generating artificial data and trying
+different parameters
+"""
+from __future__ import print_function
+import os
+import argparse
+import sys
+import numpy as np
+import time
+import pdb
+from collections import Counter
+
+#from sklearn.svc import SVC
+import sklearn.base as skb
+import sklearn.metrics as mt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.pipeline import Pipeline
+#from sklearn.externals import joblib
+#from sklearn import cross_validation
+
+
+import pylotwhale.MLwhales.featureExtraction as fex
+import pylotwhale.MLwhales.MLtools_beta as myML
+import pylotwhale.MLwhales.predictionTools as pT
+
+#import pylotwhale.MLwhales.experimentTools as exT
+from pylotwhale.MLwhales import MLEvalTools as MLvl
+### load parameters
+from pylotwhale.MLwhales.configs.params_CallTypeClf import *
+
+###################  ASSIGNMENTS  ####################
+##### OUTPUT FILES
+"""
+Runs call clf experiments from a collection of labeled cutted wave files
+
+
+
+try:
+    os.makedirs(oDir)
+except OSError:
+    pass
+out_fN = os.path.join(oDir, "scores.txt")
+
+
+if isinstance(predictionsDir, str):
+    predictionsDir = os.path.join(oDir, "predictions")
+    try:
+        os.makedirs(predictionsDir)
+    except OSError:
+        pass
+
+
+Tpipe = fex.makeTransformationsPipeline(T_settings)
+
+## clf settings
+clfStr = 'cv{}-'.format(cv)
+settingsStr = "{}-{}".format(Tpipe.string, clfStr)
+settingsStr += '-labsHierarchy_' + '_'.join(labsHierarchy)
+
+## write in out file
+out_file = open(out_fN, 'a')
+out_file.write("#WSD1\n###---------   {}   ---------###\n".format(time.strftime("%Y.%m.%d\t\t%H:%M:%S")))
+out_file.write("#" + settingsStr+'\n')
+out_file.close()
+
+## load collections
+train_coll = fex.readCols(collFi_train, colIndexes=(0,1))
+test_coll = np.genfromtxt(collFi_test, dtype=object)
+
+lt = myML.labelTransformer(clf_labs)
+"""
+
+wavColl = fex.readCols(filesDi['train'], (0,1))[:100]
+labels = [l[1] for l in wavColl]
+lt = myML.labelTransformer(labels)
+
+
+
+def runCallClfExperiment(wavColl, lt, T_settings, out_fN,
+                  cv, pipe_estimators, gs_grid, scoring=None,
+                  param=None):
+    """Runs clf experiments
+    Parameters
+    ----------
+        train_coll: list
+        test_coll: list
+        lt: ML.labelTransformer
+        T_settings: list of tuples
+        labelsHierachy: list of strings
+        cv: cv folds
+        extimators: list
+            for pipline
+        gs_grid: list
+                    
+        out_fN: str
+        returnClfs: dict, Flase => clfs are not stored
+        predictionsDir: str
+        scoring: string or sklearn.metrics.scorer
+        param: float
+            value of the param in experimet, for printing
+    """
+
+    Tpipe = fex.makeTransformationsPipeline(T_settings)
+    feExFun = Tpipe.fun
+    #### prepare DATA: collections --> X y
+    ## compute features
+    datO = fex.wavLCollection2datXy( wavColl, featExtFun=feExFun )
+    X, y_names = datO.filterInstances(lt.classes_)
+    y = lt.nom2num(y_names)
+
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y, test_size=testFrac, 
+                                                        random_state=0)
+
+
+    #### CLF
+    pipe = Pipeline(pipe_estimators)
+    gs = GridSearchCV(estimator=pipe,
+                      param_grid=gs_grid,
+                      scoring=scoring,
+                      cv=cv,
+                      n_jobs=-1)
+
+    gs = gs.fit(X_train, y_train)
+    clf_best = gs.best_estimator_
+    y_pred = clf_best.predict(X_test)
+
+    ## clf scores over test set
+    with open(out_fN, 'a') as out_file:
+        ### cv score
+        cv_sc = cross_val_score(clf_best, X_test, y_test, scoring=scoring)
+        out_file.write("{:2.2f}, {:2.2f}, {:.2f}, ".format(param, 100*np.mean(cv_sc),
+                                                            100*2*np.std(cv_sc)))
+                                                            
+        P, R, f1, _ = mt.precision_recall_fscore_support(y_test, y_pred, average='macro') # average of the scores for the call classes
+        acc = mt.accuracy_score(y_test, y_pred)
+        out_file.write("{:.2f}, {:.2f}, {:.2f}, {:.2f}\n".format(acc*100, P*100, R*100, f1*100))
+
+    return clf_best
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if False:
+    #### feature extraction object
+    feExOb = fex.wavFeatureExtraction(featConstD) # feature extraction settings
+    feature_str = feExOb.feature_str
+    feExFun = feExOb.featExtrFun()
+    
+    #### clf settings
+    clfStr = 'cv{}-{}'.format(cv, metric)
+    settingsStr = "{}-{}".format(feature_str, clfStr)
+    
+    #### write in out file
+    out_file = open(out_file_scores, 'a')
+    out_file.write("\n###---------   {}   ---------###\n".format(time.strftime("%Y.%m.%d\t\t%H:%M:%S")))
+    out_file.write("#" + settingsStr)
+    out_file.close()
+    
+    #### load collection
+    WavAnnCollection = fex.readCols(collFi_train, colIndexes = (0,1))
+    print("\ncollection:", len(WavAnnCollection),
+          "\nlast file:", WavAnnCollection[-1])
+    
+    #### compute features
+    trainDat = fex.wavAnnCollection2datXy(WavAnnCollection, feExFun) #, wavPreprocesingT=wavPreprocessingFun)
+    ## y_names train and test data
+    X, y_names = trainDat.filterInstances(labs) # train
+    lt = myML.labelTransformer(labs)
+    y = lt.nom2num(y_names)
+    labsD = lt.targetNumNomDict()
+    #### train/test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                      test_size=testFrac, 
+                                                      random_state=0)
+    
+    with open(out_file_scores, 'a') as out_file: # print details to status file
+        out_file.write("\n#TRAIN, shape {}\n".format(np.shape(X_train)))
+        out_file.write("#TEST, shape {}\n".format(np.shape(X_test))) 
+    
+        ## more info
+        #out_file.write("#X {}, y {}\n".format( np.shape(X), np.shape(y)))
+        out_file.write("#Target dict {}\t{}\n".format(labsD, trainDat.targetFrequencies()))
+    
+    
+    ### grid search
+    
+    gs = GridSearchCV(estimator=pipe_estimators,
+                      param_grid=gs_grid,
+                      scoring=metric,
+                      cv=cv,
+                      n_jobs=-1)
+    
+    gs = gs.fit(X_train, y_train)
+    
+    with open(out_file_scores, 'a') as out_file:
+        out_file.write(MLvl.gridSearchresults(gs))
+    
+    
+    print(out_file_scores)
+    
---
+++ pylotwhale/MLwhales/runExperiments_callClf-cutWavs.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 18 18:05:03 20156
+#!/usr/bin/python
+@author: florencia
+
+Runs call classification experiments generating artificial data and trying
+different parameters
+"""
+
+from __future__ import print_function
+import os
+import argparse
+import time
+
+import sys
+import numpy as np
+
+from pylotwhale.MLwhales.configs.params_CallTypeClf import *
+from pylotwhale.MLwhales.configs.iter_params_callClf import experimentsControlParams
+import pylotwhale.MLwhales.MLtools_beta as myML
+from pylotwhale.MLwhales.experiment_callClf import runCallClfExperiment
+
+"""
+import pylotwhale.MLwhales.featureExtraction as fex
+from pylotwhale.MLwhales.configs.params_WSD1 import *
+from pylotwhale.MLwhales.experiment_WSD1 import runExperiment
+from pylotwhale.MLwhales.configs.iter_params_WSD import experimentsControlParams
+from pylotwhale.MLwhales import MLEvalTools as MLvl
+import pylotwhale.MLwhales.MLtools_beta as myML
+"""
+
+parser = argparse.ArgumentParser(description='Runs controled experiment.')
+parser.add_argument("-cnv", "--controlVariable", type=str,
+                    help="name of the experiment controled variable, "
+                         "eg: NFFT, NArtificialSamples,"
+                         "etc. See iter_params.py.",
+                    default="NFFT")
+
+#parser.add_argument("-vals", '--paramVals',# nargs='+',help='values of the experiment')
+args = parser.parse_args()
+
+#### Control parameters
+controlVariable = args.controlVariable
+print("Control variable: ", controlVariable)
+controlParamO = experimentsControlParams(controlVariable)
+
+## variables
+iterParam = controlParamO.parameter
+exp_grid_params = controlParamO.controlParams
+settingsDi = controlParamO.paramDict
+updateTestSet = controlParamO.updateTestSet
+expSettingsStr = controlParamO.settingsStr
+
+## more settings
+fex.readCols(filesDi['train'], (0,1))[:100]
+wavColl = fex.readCols(filesDi['train'], (0,1))[:100]
+call_labels = [l[1] for l in wavColl]
+lt = myML.labelTransformer(call_labels)
+
+
+##### OUTPUT FILES
+oDir = os.path.join(filesDi['outDir'], iterParam)
+
+try:
+    os.makedirs(oDir)
+except OSError:
+    pass
+out_fN = os.path.join(oDir, "scores.txt")
+
+Tpipe = fex.makeTransformationsPipeline(T_settings)
+
+## clf settings
+clfStr = 'cv{}_{}'.format(cv, metric)
+settingsStr = "{}-{}".format(Tpipe.string, clfStr)
+
+## write in out file
+out_file = open(out_fN, 'a')
+out_file.write("# call-clf experiment {}\n".format(expSettingsStr))
+out_file.write("###---------   {}   ---------###\n".format(time.strftime("%Y.%m.%d\t\t%H:%M:%S")))
+out_file.write("#" + settingsStr+'\n')
+out_file.close()
+
+
+for param in exp_grid_params:
+    settingsDi[iterParam] = param
+    print(param, T_settings)
+    runCallClfExperiment(wavColl, lt, T_settings, out_fN, 
+                         cv, pipe_estimators, gs_grid, metric, param=param)
+
