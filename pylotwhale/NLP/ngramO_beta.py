ad908ab07dd86312824849f113ef126ae20622a7
Florencia Noriega <flo@nld.ds.mpg.de>
2017-01-09 17:33:30 +0800

    tools for exploring timing patterns and chunk structure added

--- pylotwhale/NLP/ngramO_beta.py
+++ pylotwhale/NLP/ngramO_beta.py
@@ -139,9 +139,8 @@ def barPltsSv(y, labs, figN='', figSz=(10, 3), yL='# bigrams',
         print figN
 
 
-### TIMING
+### TIMING ####
 ### stats plots
-
 
 def pl_ic_bigram_times(df0, my_bigrams, ignoreKeys='default', label='call', oFig=None, 
                        violinInner='box', yrange='default', ylabel='time (s)',
@@ -157,9 +156,9 @@ def pl_ic_bigram_times(df0, my_bigrams, ignoreKeys='default', label='call', oFig
         violinInner : viloin lor parameter
         yrange : 'default' (0, mu*2)
     '''
-        
+
     if ignoreKeys == 'default': ignoreKeys = ['_ini', '_end']
-        
+
     topBigrams = daT.removeFromList(daT.returnSortingKeys(Counter(my_bigrams)), ignoreKeys)
     bigrTimes=[]
     bigrNames=[]
@@ -184,6 +183,48 @@ def pl_ic_bigram_times(df0, my_bigrams, ignoreKeys='default', label='call', oFig
     plt.ylabel(ylabel)
     plt.savefig(oFig)  
 
+
+### chuck structure
+
+def Ngrams_distributionDt_list(df_tapes, Dtvec, seqLabel='call', time_param='ict_end_start'):
+    """Returns the distribution of ngrams (list) at the times specified by Dtvec"""
+    ngramFreqsDtvec_li = []
+    for Dt in Dtvec: # delta t iteration
+        seqsLi = []
+        for thisdf in df_tapes.values(): # tape iteration
+            seqsLi += aa.df2listOfSeqs( thisdf, Dt=(None, Dt), l=seqLabel, 
+                                       time_param=time_param) # sequences list (of lists)
+
+        seqlens = [ len(item) for item in seqsLi] # Ngrams --> N
+        ngramFreqsDtvec_li.append(np.bincount(seqlens))
+
+    return ngramFreqsDtvec_li
+
+def flattenlists_2darray(liofli):
+    """Maps a list of lists into a 2d ndarray filling with zeroes"""
+    n_rows = len(liofli)
+    n_cols = len(liofli[-1])
+    twoDarray = np.zeros((n_rows, n_cols)) # inicialise
+    
+    for i in np.arange(n_rows):
+        item = liofli[i]
+        twoDarray[i,:len(item)] = item
+        
+    return twoDarray
+
+def Ngrams_distributionDt_ndarray(df_tapes, Dtvec, seqLabel='call', time_param='ict_end_start'):
+    """Returns the distributions of ngrams at different values of tau as an 2d array
+    where the column number marches the number of ngrams and the rows value of tau"""
+    ngramsDt_li = Ngrams_distributionDt_list(df_tapes, Dtvec, seqLabel=seqLabel, 
+                                             time_param=time_param)
+    return flattenlists_2darray(ngramsDt_li)
+
+
+def NgramsDist2callsInNgramsDist(ngrams_dist):
+    """transforms a ngram_distritribution matrix into a call in ngrams distribution"""
+    ngrams_nCalls_arr =  np.arange(ngrams_dist.shape[1])
+    return ngrams_dist*ngrams_nCalls_arr
+    
 
 ##### NLTK - pandas - related ngram code  #####
 
