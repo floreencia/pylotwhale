06d8c533ea5572cd354b5b34dd9172f2f87aaf39
Florencia Noriega <flo@chiliques.mprg.ds.mpg.de>
2017-03-11 14:34:29 +0100

    Tpipe_settings_and_header added

--- pylotwhale/MLwhales/experiment_callClf.py
+++ pylotwhale/MLwhales/experiment_callClf.py
@@ -117,6 +117,20 @@ def print_exeriment_header(out_fN, expSettingsStr, settingsStr, trainFi,
     return out_fN
 
 
+def Tpipe_settings_and_header(Tpipe, sep=", "):
+    """returns two strings with the instructions in Tpipe (pipeline of Transformations)
+    header_str, settings_str"""
+    for step in Tpipe.step_sequence:
+        header.append( step)
+        values.append(Tpipe.steps[step].name)
+        for ky in Tpipe.steps[step].settingsDict.keys():
+            header.append( ky)
+            values.append( Tpipe.steps[step].settingsDict[ky] )
+            
+    header_str = ("{}".format(sep).join(header))
+    settings_str = "{}".format(sep).join(["{}".format(item) for item in values])
+    return header_str, settings_str
+
 def callClfExperiment(wavColl, lt, Tpipe, out_fN, testFrac,
                          cv, pipe_estimators, gs_grid, 
                          filterClfClasses, scoring=None,
@@ -146,7 +160,7 @@ def callClfExperiment(wavColl, lt, Tpipe, out_fN, testFrac,
     #Tpipe = fex.makeTransformationsPipeline(T_settings)
     feExFun = Tpipe.fun
     
-    fs=Tpipe.steps['Audio_features'].settingsDict['fs']
+    fs = Tpipe.steps['Audio_features'].settingsDict['fs']
     #### prepare DATA: collections --> X y
     ## compute features
     datO = fex.wavLCollection2datXy( wavColl, fs=fs, featExtFun=feExFun )
@@ -170,16 +184,21 @@ def callClfExperiment(wavColl, lt, Tpipe, out_fN, testFrac,
     y_pred = clf_best.predict(X_test)
 
     ## clf scores over test set
+
+    settings_str = Tpipe_settings_and_header(Tpipe)[1]
     with open(out_fN, 'a') as out_file:
-        ### cv score
+        ### cv scores
         cv_sc = cross_val_score(clf_best, X_train, y_train, scoring=scoring)
         out_file.write("{:}, {:2.2f}, {:.2f}, ".format(param, 100*np.mean(cv_sc),
                                                             100*2*np.std(cv_sc)))
-                                                            
+        ### test: ACC, P, R, f1
         P, R, f1, _ = mt.precision_recall_fscore_support(y_test, y_pred, average='macro') # average of the scores for the call classes
         acc = mt.accuracy_score(y_test, y_pred)
-        out_file.write("{:.2f}, {:.2f}, {:.2f}, {:.2f}\n".format(acc*100, P*100, R*100, f1*100))
-
+        out_file.write("{:.2f}, {:.2f}, {:.2f}, {:.2f}".format(acc*100, P*100, R*100, f1*100))
+        
+        ### settings
+        out_file.write("{}\n".format(settings_str))
+        
     return clf_best.fit(X, y)
 
 
