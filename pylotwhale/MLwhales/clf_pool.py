00e0106ccd5d13361303a202512637dc6b3f0bac
Florencia Noriega <flo@nld.ds.mpg.de>
2018-01-31 23:22:24 +0100

    fix random_forest GridSearchCV parameters

--- pylotwhale/MLwhales/clf_pool.py
+++ pylotwhale/MLwhales/clf_pool.py
@@ -12,10 +12,11 @@ import numpy as np
 
 
 class clfSettings():
-    def __init__(self, clf_name, fun, grid_params_di):
+    def __init__(self, clf_name, fun, grid_params_di, pipeStep_name='clf'):
         self.clf_name = clf_name
         self.fun = fun
         self.grid_params_di = grid_params_di
+        self.pipeStep_name = pipeStep_name
 
 
 #SVC rbf
@@ -31,7 +32,9 @@ pen_range = [ 0.1, 1.0, 10.0, 100]
 param_grid_di = {'clf__kernel': ['linear'],
                  'clf__C': pen_range}
                 
-svc_l =  clfSettings('svc_linear', SVC(), param_grid_di)
+svc_l =  clfSettings('svc_linear', 
+                     SVC(), 
+                    param_grid_di)
 
 #SVC rbf w/ probs
 gamma_range = [ 0.1, 1.0, 10.]
@@ -39,13 +42,15 @@ pen_range = [ 0.1, 1.0, 10.0, 100]
 param_grid_di = {'clf__C': pen_range,
                 'clf__gamma': gamma_range}
 
-svc_rbf_p =  clfSettings('svc_rbf_p', SVC(probability=True), param_grid_di)
+svc_rbf_p =  clfSettings('svc_rbf_p', 
+                         SVC(probability=True), 
+                        param_grid_di)
 
 #RF
-ests_range = np.array([50, 100])
-param_grid_di = {}#"max_depth": [3, None],
-              #"bootstrap": [True, False]}
-#              "n_estimators": ests_range}
+ests_range = [50, 100]
+param_grid_di = {"clf__max_depth": [3, None],
+                 "clf__bootstrap": [True, False],
+                 "clf__n_estimators": ests_range}
 
 random_forest = clfSettings('rf', 
                             RandomForestClassifier(max_depth=None, bootstrap=True), 
