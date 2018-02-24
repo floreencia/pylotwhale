3f18f452b825e2b1e588b0c5adf7bfbfd7c760c8
Florencia Noriega <flo@nld.ds.mpg.de>
2018-02-24 20:06:40 +0100

    change name of kwarg

--- pylotwhale/MLwhales/tests/test_featureExtraction.py
+++ pylotwhale/MLwhales/tests/test_featureExtraction.py
@@ -12,20 +12,21 @@ wf = np.sin(np.linspace(0, 100, 10000))
 fs = 100
 
 def test_Transformation_fun():
-        audioFeatDi = {"NFFT":2**9, "overlap" : 0.5, "sRate":fs }
-        T1 = fex.Transformation('spectral', audioFeatDi)
-        fun1 = T1.fun
-        M1 = fun1(wf)
+    audioFeatDi = {"NFFT":2**9, "overlap" : 0.5, "fs":fs }
+    T1 = fex.Transformation('spectral', audioFeatDi)
+    fun1 = T1.fun
+    M1 = fun1(wf)
+    
+    summDict = {'n_textWS':5, 'normalise':False}
+    T2 = fex.Transformation('walking', summDict)
+    fun2 = T2.fun
+    M2 = fun2(M1)
+    
+    tList = [('audio_features', T1), ('summ', T2)]
+    Tpipe = fex.TransformationsPipeline(tList)
+    Mpipe = Tpipe.fun(wf)
+    
+    np.testing.assert_almost_equal(M2, Mpipe, decimal=6)
 
-        summDict = {'n_textWS':5, 'normalise':False}
-        T2 = fex.Transformation('walking', summDict)
-        fun2 = T2.fun
-        M2 = fun2(M1)
-
-        tList = [('audio_features', T1), ('summ', T2)]
-        Tpipe = fex.TransformationsPipeline(tList)
-        Mpipe = Tpipe.fun(wf)
-
-        np.testing.assert_almost_equal(M2, Mpipe, decimal=6)
-
-
+    
+    
