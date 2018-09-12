3105643c1423dc45f540340feb4dd24d08565378
Florencia Noriega <flo@nld.ds.mpg.de>
2018-09-12 18:05:54 +0200

    delet distanceTools and move its function to annFeatureExtractionTools

--- pylotwhale/NLP/annFeatureExtractionTools.py
+++ pylotwhale/NLP/annFeatureExtractionTools.py
@@ -52,3 +52,28 @@ def df2Xy(df, groupingKey='tape', call='note', sep=' ',
     return X, np.array(y)
 
 
+def df2groupedCorpus(df, groupingKey='tape', call='note', sep='\s'):
+    """returns the feature list from a dataframe grouping  
+    the value of the grouping key
+    Parameteres
+    -----------
+    df: pandas dataframe
+    groupingKey: str
+    call: str
+    Returns
+    --------
+    X_str: list of strings (n_instances)
+        feateure matrix to use as input of CountVectorizer
+    y: list, (n_instances)
+        labels of the instances from the grouping key
+    """
+    ## group dataframe
+    df_dict = daT.dictOfGroupedDataFrames(df, groupingKey=groupingKey)
+    X_str = [] # feature list
+    y = [] # group label
+    for ky in df_dict.keys():
+        y.append(ky) # gro
+        X_str.append(sep.join( df_dict[ky][call].values))
+    return X_str, y
+
+
